51b11b2f78dbe3053318034ceadf522f985e8889
Jiho Tahk <jihotahk@gmail.com>
2017-03-19 21:44:48 -0500

    fixed functionality of test and message

--- request-gif.js
+++ request-gif.js
@@ -3,53 +3,59 @@ $(document).ready(function() {
     $("#form-gif-request").submit(fetchAndDisplayGif);
 });
 
-
-/**
- * sends an asynchronous request to Giphy.com aksing for a random GIF using the
- * user's search term (along with "jackson 5")
- *
- * upon receiving a response from Giphy, updates the DOM to display the new GIF
- */
 function fetchAndDisplayGif(event) {
 
     // prevent get request default action
     event.preventDefault();
 
-    // get the user's input text from the DOM
-    var searchQuery = $('input[name="tag"]').val();
+    // first check riddle - if correct, run query, otherwise display error
+    var riddle_ans = $('input[name="riddle"]').val();
 
-    // configure a few parameters to attach to our request
-    var params = {
-        api_key: "dc6zaTOxFJmzC",
-        tag : "jackson 5 "+searchQuery
+    if (riddle_ans == 5) {
+        // get rid of any error messages
+        $('#riddle-feedback').empty()
+
+        // get the user's input text from the DOM
+        var searchQuery = $('input[name="tag"]').val();
+
+        // configure a few parameters to attach to our request
+        var params = {
+            api_key: "dc6zaTOxFJmzC",
+            tag : "jackson 5 "+searchQuery
+        };
+        // make an ajax request for a random GIF
+        $.ajax({
+            url: "https://api.giphy.com/v1/gifs/random",
+            data: params, // attach those extra parameters onto the request
+            success: function(response) {
+                // jQuery passes us the `response` variable, a regular javascript object created from the JSON the server gave us
+                // retrieve the image url
+                var url = response['data']['image_url'];
+                // set the image src attribute to the retrieved url
+                $('#gif').attr('src', url);
+                // toggle the status so it hides message and displays gif
+                setGifLoadedStatus(true);
+            },
+            error: function() {
+                // if something went wrong, the code in here will execute instead of the success function
+
+                // give the user an error message
+                $("#feedback").text("Sorry, could not load GIF. Try again!");
+                setGifLoadedStatus(false);
+                }
+        });
+
+    } else if (riddle_ans =="") {
+        setGifDeletedStatus(false);
+        $('#riddle-feedback').text('Please answer the riddle above.');
+    } else {
+        setGifDeletedStatus(false);
+        $('#riddle-feedback').text('No gifs for you.');
     };
-    // make an ajax request for a random GIF
-    $.ajax({
-        url: "https://api.giphy.com/v1/gifs/random",
-        data: params, // attach those extra parameters onto the request
-        success: function(response) {
-            // jQuery passes us the `response` variable, a regular javascript object created from the JSON the server gave us
-            // retrieve the image url
-            var url = response['data']['image_url'];
-            // set the image src attribute to the retrieved url
-            $('#gif').attr('src', url);
-            // toggle the status so it hides message and displays gif
-            setGifLoadedStatus(true);
-        },
-        error: function() {
-            // if something went wrong, the code in here will execute instead of the success function
-
-            // give the user an error message
-            $("#feedback").text("Sorry, could not load GIF. Try again!");
-            setGifLoadedStatus(false);
-        }
-    });
 
     // give user feedback while the ajax request is running
     $("#feedback").text("Loading...");
-
 }
-
 
 /**
  * toggles the visibility of UI elements based on whether a GIF is currently loaded.
@@ -59,4 +65,9 @@ function fetchAndDisplayGif(event) {
 function setGifLoadedStatus(isCurrentlyLoaded) {
     $("#gif").attr("hidden", !isCurrentlyLoaded);
     $("#feedback").attr("hidden", isCurrentlyLoaded);
+}
+
+function setGifDeletedStatus(isCurrentlyLoaded) {
+    $("#gif").attr("hidden", !isCurrentlyLoaded);
+    $("#feedback").attr("hidden", !isCurrentlyLoaded);
 }
